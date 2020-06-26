b9b90b17fb2cceab0399bbdc7b1d45bf62e806ab
Jan Piotrowski <piotrowski+github@gmail.com>
2020-06-26 11:18:49 +0200

    chore(config): Fix siteUrl and update title, meta-title and -description (#17)
    * chore(config): Fix siteUrl and update title, meta-title and -description
    
    * Update index.mdx

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 const config = {
   gatsby: {
     pathPrefix: '',
-    siteUrl: 'https://dataguide.prisma.com', // replace with correct site
+    siteUrl: 'https://dataguide.prisma.io', // replace with correct site
   },
   redirects: [],
   header: {
     logoLink: '/', // replace with correct site
-    title: "Prisma's Data Guide",
+    title: "Prisma's Data Guide - Educational articles to make databases more approachable",
     search: {
       indexName: process.env.GATSBY_ALGOLIA_INDEX_NAME,
       algoliaAppId: process.env.GATSBY_ALGOLIA_APP_ID,
@@ -15,9 +15,9 @@ const config = {
     },
   },
   siteMetadata: {
-    title: "Prisma's Data Guide",
-    description: "Prisma's Data Guide",
-    keywords: "Prisma's Data Guide, dataguide, howtodata",
+    title: "Prisma's Data Guide - Educational articles to make databases more approachable",
+    description: "Learn how databases work, how to choose the right one, and how to use databases with your applications to their full potential.",
+    keywords: "Prisma's Data Guide, prisma, database, mysql, postgres, postgresql, dataguide",
     docsLocation: 'https://github.com/prisma/dataguide/tree/master/content',
     twitter: {
       site: '@prisma',
