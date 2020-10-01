f0ddbc5999d0fc14b8d57f03113f901f59d146af
Nilufar Bava <nilubava@gmail.com>
2020-10-01 16:18:07 +0200

    feat: Edit on github (#91)

--- src/components/pageBottom.tsx
+++ src/components/pageBottom.tsx
@@ -1,20 +1,20 @@
 import * as React from 'react'
 import styled from 'styled-components'
-import Up from '../icons/Up'
-import Down from '../icons/Down'
+// import Up from '../icons/Up'
+// import Down from '../icons/Down'
 import Link from './link'
-import config from '../../config'
-import { ButtonWrapper } from './customMdx/button'
-import Twitter from '../icons/Twitter'
-import { useLocation } from '@reach/router'
+// import config from '../../config'
+// import { ButtonWrapper } from './customMdx/button'
+// import Twitter from '../icons/Twitter'
+// import { useLocation } from '@reach/router'
 
-const sentiments: any = {
-  unhappy: 'Unhappy',
-  happy: 'Happy',
-}
+// const sentiments: any = {
+//   unhappy: 'Unhappy',
+//   happy: 'Happy',
+// }
 
-const gitIssueUrl = `https://github.com/prisma/prisma2-docs/issues/new?labels=kind/docs,content`
-const twitterShareUrl = `https://twitter.com/intent/tweet?text=I%27ve%20found%20this%20%40prisma%20docs%20page%20helpful%21%20`
+// const gitIssueUrl = `https://github.com/prisma/dataguide/issues/new?labels=kind/docs,content`
+// const twitterShareUrl = `https://twitter.com/intent/tweet?text=I%27ve%20found%20this%20%40dataguide%20page%20helpful%21%20`
 
 const PageBottomWrapper = styled.div`
   display: flex;
@@ -45,95 +45,96 @@ const PageBottomWrapper = styled.div`
   }
 `
 
-const Feedback = styled.div`
-  h4 {
-    text-transform: uppercase;
-    font-weight: bold;
-    letter-spacing: 0.01em;
-    text-transform: uppercase;
-    color: var(--list-bullet-color) !important;
-  }
-  .sentiments {
-    button {
-      background: transparent;
-      border: 0;
-      &:hover svg {
-        border-radius: 50%;
-        background: rgba(204, 217, 223, 0.5);
-      }
-    }
-  }
-  @media (min-width: 0px) and (max-width: 767px) {
-    order: 2;
-  }
-`
+// const Feedback = styled.div`
+//   h4 {
+//     text-transform: uppercase;
+//     font-weight: bold;
+//     letter-spacing: 0.01em;
+//     text-transform: uppercase;
+//     color: var(--list-bullet-color) !important;
+//   }
+//   .sentiments {
+//     button {
+//       background: transparent;
+//       border: 0;
+//       &:hover svg {
+//         border-radius: 50%;
+//         background: rgba(204, 217, 223, 0.5);
+//       }
+//     }
+//   }
+//   @media (min-width: 0px) and (max-width: 767px) {
+//     order: 2;
+//   }
+// `
 
-const Wrapper = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  align-items: flex-start;
-  cursor: default;
-`
+// const Wrapper = styled.div`
+//   display: flex;
+//   align-items: center;
+//   justify-content: space-between;
+//   align-items: flex-start;
+//   cursor: default;
+// `
 
-const Content = styled.div`
-  flex: 1;
-`
+// const Content = styled.div`
+//   flex: 1;
+// `
 
-const Title = styled.div`
-  margin: 0 0 16px 0 !important;
-  font-weight: 600;
-  font-size: 16px;
-`
-const P = styled.p`
-  color: var(--list-bullet-color) !important;
-  margin-top: 0;
-`
+// const Title = styled.div`
+//   margin: 0 0 16px 0 !important;
+//   font-weight: 600;
+//   font-size: 16px;
+// `
+// const P = styled.p`
+//   color: var(--list-bullet-color) !important;
+//   margin-top: 0;
+// `
 
-const Button = styled(ButtonWrapper)`
-  display: inline-flex !important;
-  svg {
-    margin-right: 10px;
-    path {
-      stroke: var(--white-color);
-    }
-  }
-  @media (min-width: 0px) and (max-width: 767px) {
-    font-size: 12px;
-    text-transform: none;
-    svg {
-      width: 14px;
-    }
-  }
-`
+// const Button = styled(ButtonWrapper)`
+//   display: inline-flex !important;
+//   svg {
+//     margin-right: 10px;
+//     path {
+//       stroke: var(--white-color);
+//     }
+//   }
+//   @media (min-width: 0px) and (max-width: 767px) {
+//     font-size: 12px;
+//     text-transform: none;
+//     svg {
+//       width: 14px;
+//     }
+//   }
+// `
 
 const PageBottom = ({ editDocsPath }: any) => {
-  const [submitted, setSubmitted] = React.useState(false)
-  const [sentiment, setSentiment] = React.useState(sentiments['happy'])
+  // const [submitted, setSubmitted] = React.useState(false)
+  // const [sentiment, setSentiment] = React.useState(sentiments['happy'])
 
-  let location = useLocation()
-  const pageUrl = location ? location.pathname : '/'
-  const currentDocsPageURL = encodeURIComponent(location ? location.href : '/')
+  // let location = useLocation()
+  // const pageUrl = location ? location.pathname : '/'
+  // const currentDocsPageURL = encodeURIComponent(location ? location.href : '/')
 
-  const sendFeedback = async (selectedSentiment: string) => {
-    const body = JSON.stringify({ pageUrl, sentiment: sentiments[selectedSentiment] })
-    const requestOptions: any = {
-      method: 'POST',
-      body,
-    }
-    await fetch(config.feedback.function_name, requestOptions)
-  }
+  // const sendFeedback = async (selectedSentiment: string) => {
+  //   const body = JSON.stringify({ pageUrl, sentiment: sentiments[selectedSentiment] })
+  //   const requestOptions: any = {
+  //     method: 'POST',
+  //     body,
+  //   }
+  //   await fetch(config.feedback.function_name, requestOptions)
+  // }
 
-  const handleSentiment = (e: any) => {
-    const selectedSentiment = e.currentTarget.id
-    setSentiment(sentiments[selectedSentiment])
-    setSubmitted(true)
-    sendFeedback(selectedSentiment)
-  }
+  // const handleSentiment = (e: any) => {
+  //   const selectedSentiment = e.currentTarget.id
+  //   setSentiment(sentiments[selectedSentiment])
+  //   setSubmitted(true)
+  //   sendFeedback(selectedSentiment)
+  // }
 
   return (
     <PageBottomWrapper>
-      {!submitted ? (
+      <div/> 
+      {/* {!submitted ? (
         <Feedback>
           <h4>Was this helpful?</h4>
           <div className="sentiments">
@@ -157,7 +158,7 @@ const PageBottom = ({ editDocsPath }: any) => {
                   Tell us why on GitHub!
                 </P>
                 <Button target="_blank" href={gitIssueUrl} type="primary" color="dark">
-                  Tell us On Github
+                  Tell us On GitHub
                 </Button>
               </>
             ) : (
@@ -167,16 +168,16 @@ const PageBottom = ({ editDocsPath }: any) => {
                   type="primary"
                   href={`${twitterShareUrl}${currentDocsPageURL}`}
                 >
-                  <Twitter /> Share the Prisma docs on Twitter
+                  <Twitter /> Share Dataguide on Twitter
                 </Button>
               </>
             )}
           </Content>
         </Wrapper>
-      )}
+      )} */}
       {editDocsPath && (
         <Link className="edit-git" to={`${editDocsPath}`}>
-          Edit this page on Github
+          Edit this page on GitHub
         </Link>
       )}
     </PageBottomWrapper>
