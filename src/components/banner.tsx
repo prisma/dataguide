f79c5959783a539a68ff2297c0fa1d6e6e118a60
Nilu <nilubava@gmail.com>
2021-11-18 17:14:26 +0100

    feat: pdp banner

---
+++ src/components/banner.tsx
@@ -0,0 +1,39 @@
+import * as React from 'react'
+import styled from 'styled-components'
+
+const BannerWrapper = styled.div`
+  background: linear-gradient(90deg, #dbf3ed 0%, #ebf4ff 100%);
+  color: #4a5568;
+  padding: 16px;
+  > div {
+    display: flex;
+    text-align: center;
+    justify-content: center;
+    align-items: center;
+    max-width: 1200px;
+    margin: 0 auto;
+  }
+  @media (max-width: 1000px) {
+    font-size: 14px;
+  }
+`
+const BannerText = styled.div`
+  margin-right: 12px;
+
+  a {
+    text-decoration: none;
+    color: #4a5568;
+  }
+`
+const Banner = () => (
+  <BannerWrapper>
+    <BannerText>
+      <a href="/dataplatform">
+        Manage your application data in one place with the <strong>Prisma Data Platform</strong>.
+        Learn more ->
+      </a>
+    </BannerText>
+  </BannerWrapper>
+)
+
+export default Banner
