e44f2663047a2dc27fbc16de5bbb461d5330b613
Nilu <nilubava@gmail.com>
2022-08-17 17:07:04 +0200

    fix: remove trailing slahes in internal links

--- plugins/gatsby-remark-to-absoluteurl/index.js
+++ plugins/gatsby-remark-to-absoluteurl/index.js
@@ -6,7 +6,7 @@ function withPathPrefix(url, pathPrefix) {
   return prefixed.replace(/\/\//, '/')
 }
 
-const pathSep = path.sep || '/'
+const pathSep = '/'
 
 module.exports = function plugin(
   { markdownAST, markdownNode, pathPrefix, getNode },
@@ -14,6 +14,7 @@ module.exports = function plugin(
 ) {
   function visitor(node) {
     node.originalUrl = node.url
+    
     if (
       markdownNode.fields &&
       markdownNode.fields.slug &&
@@ -26,7 +27,7 @@ module.exports = function plugin(
       const newUrl = path
         .resolve(
           markdownNode.fields.slug
-            .replace(`${pathSep}index`, '')
+            .replace(new RegExp('\\b' + `${pathSep}index` + '\\b'), '')
             .replace(/\d{2,}-/g, '')
             .replace(/\/$/, '')
             .split(pathSep)
@@ -34,10 +35,35 @@ module.exports = function plugin(
             .join(pathSep) || '/',
           node.url
         )
-        .replace(/\/?(\?|#|$)/, '/$1')
+        .replace(/\/?(\?|$)/, '/$1')
+        .replace(/\/$/, '')
 
-      const isRedirectPath = redirects.find(url => newUrl.includes(url.from))
-      node.url = withPathPrefix(isRedirectPath ? newUrl.replace(isRedirectPath.from, isRedirectPath.to) : newUrl, pathPrefix)
+      if (/^..\\/.test(newUrl)) {
+        //Code specifically for local run, to fix broken links on
+        let newUrl2 = path.resolve(
+          markdownNode.fields.slug.replace(/(\/.+)\/.*/, '$1').replace(/\/\d{2,}-/g, '/'),
+          node.url
+        )
+
+        newUrl2 = newUrl2
+          .replace(/\\/g, '/')
+          .slice(2)
+          .replace(/(^.*)#.*/, '$1')
+
+        const isRedirectPath = redirects.find((url) => newUrl2.includes(url.from))
+        if (isRedirectPath) newUrl2 = isRedirectPath.to
+
+        let hashVal = node.url.match(/#.*/)
+        if (hashVal) newUrl2 += hashVal[0]
+
+        node.url = newUrl2.replace(/^([^#]*)$/, '$1/')
+      } else {
+        const isRedirectPath = redirects.find((url) => newUrl.includes(url.from))
+        node.url = withPathPrefix(
+          isRedirectPath ? newUrl.replace(isRedirectPath.from, isRedirectPath.to) : newUrl,
+          pathPrefix
+        )
+      }
     }
   }
 
