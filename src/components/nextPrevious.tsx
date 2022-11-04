eb5a71976895f12aa0abe83db62534a8eccbd204
Nilu <nilubava@gmail.com>
2022-11-04 15:51:53 +0100

    fix: config

--- src/components/nextPrevious.tsx
+++ src/components/nextPrevious.tsx
@@ -148,13 +148,7 @@ const NextPrevious = ({ slug }: any) => {
           <div className="previous">
             <div className="text">
               <span className="direction">Previous</span>
-              <Link
-                to={
-                  urlGenerator(nav[currentIndex - 1].url) === '/'
-                    ? ''
-                    : urlGenerator(nav[currentIndex - 1].url)
-                }
-              >
+              <Link to={urlGenerator(nav[currentIndex - 1].url).replace(/\/$/, '')}>
                 <span className="icon">
                   <ArrowLeft color="#3182CE" />
                 </span>
