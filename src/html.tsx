9ec83bff708c2a7a0681023a5a6abe5aa9cc8fd2
Julie M <70962362+meletj@users.noreply.github.com>
2024-05-24 18:26:53 +0200

    update cookie script (#476)
    * update cookie script
    
    * Update html.tsx
    
    * Update html.tsx

--- src/html.tsx
+++ src/html.tsx
@@ -6,17 +6,13 @@ const HTML = (props: any) => {
     <html {...props.htmlAttributes}>
       <head>
         <meta charSet="utf-8" />
-        {/* OneTrust Cookies Consent Notice start for prisma.io  */}
 
         <script
-          src="https://cdn.cookielaw.org/scripttemplates/otSDKStub.js"
-          data-document-language="true"
+          id="cookies"
+          src="https://app.enzuzo.com/apps/enzuzo/static/js/__enzuzo-cookiebar.js?uuid=5606ab18-eb9a-11ee-98cc-b303d4429aa8"
           type="text/javascript"
-          charSet="UTF-8"
-          data-domain-script="22c2e2c0-3df0-4958-8672-1194370ee230"
         ></script>
-        <script type="text/javascript">function OptanonWrapper() {}</script>
-        {/* OneTrust Cookies Consent Notice end for prisma.io */}
+        
         <meta httpEquiv="x-ua-compatible" content="ie=edge" />
         {props.headComponents}
       </head>
