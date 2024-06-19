688108d07581dc63cda61b0c60c9f2178fbe142d
Julie M <70962362+meletj@users.noreply.github.com>
2024-06-19 17:28:40 +0200

    remove enzuzo as it's now done via google tag manager

--- src/html.tsx
+++ src/html.tsx
@@ -6,12 +6,6 @@ const HTML = (props: any) => {
     <html {...props.htmlAttributes}>
       <head>
         <meta charSet="utf-8" />
-
-        <script
-          id="cookies"
-          src="https://app.enzuzo.com/apps/enzuzo/static/js/__enzuzo-cookiebar.js?uuid=5606ab18-eb9a-11ee-98cc-b303d4429aa8"
-          type="text/javascript"
-        ></script>
         
         <meta httpEquiv="x-ua-compatible" content="ie=edge" />
         {props.headComponents}
