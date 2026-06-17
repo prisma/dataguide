45653e28033ede892d24779d256ae20af2e684d7
Nurul Sundarani <sundarani@prisma.io>
2026-06-17 19:08:17 +0530

    PostHog Integration

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -1,11 +1,14 @@
 const { init, trackPage } = require('./src/utils/stats')
+const { init: initPostHog, trackPage: trackPostHogPage } = require('./src/utils/posthog')
 const { goToNav } = require('./src/utils/goToNavItem')
 
 exports.onClientEntry = () => {
   init()
+  initPostHog()
 }
 
 exports.onRouteUpdate = ({ location }) => {
   trackPage(location.pathname)
+  trackPostHogPage(location.pathname)
   goToNav(location.pathname)
 }
